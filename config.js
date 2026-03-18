ae64c685c87f3e07a53581e58c1c35b2b833fca2
Sena <Sena@MEDCTMAUSB24750>
2026-03-18 16:21:27 -0500

    Modificacion

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Archivo: src/config.js
 // Configuración global del frontend Agenda ADSO
 
-export const API_BASE_URL = "https://agenda-adso-api-h4t0.onrender.com";
+export const API_BASE_URL = "https://agenda-adso-api.onrender.com/contactos";
 
 export const APP_INFO = {
   titulo: "Agenda ADSO v10",
